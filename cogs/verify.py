51e74f4e40625d961e646b74f05983154acf96f7
Kolen <you@example.com>
2021-06-14 23:20:35 +0300

    Update verify.py

--- cogs/verify.py
+++ cogs/verify.py
@@ -17,7 +17,7 @@ class Verify(commands.Cog):
         if reaction.message_id == self.config['verify']['message_id']:
             if reaction.emoji.name == self.config['verify']['reaction']:
                 embed=discord.Embed(color=0x0be541)
-                embed.add_field(name="Koper Verify", value="Привет, я бот регистрации в системе Koper Verify для того что бы зарегестрироваться придумай пароль и отправь его мне", inline=False)
+                embed.add_field(name="Koper Verify", value="Здравствуй, я бот регистрации в системе Koper Verify, для того что бы зарегистрироваться придумайте пароль и отправьте его мне.", inline=False)
                 await reaction.member.send(embed=embed)
     
     @commands.Cog.listener()
@@ -32,7 +32,7 @@ class Verify(commands.Cog):
                 register = self.api.connect.register(code, ctx.content)
                 if register['success']:
                     embed=discord.Embed(color=0x0be541)
-                    embed.add_field(name="Koper Verify", value="Вы успешно зарегестрировались, войти вы можете на сайте: http://lavomerka.ml/login/", inline=False)
+                    embed.add_field(name="Koper Verify", value="Вы успешно зарегестрировались, теперь вы можете авторизоваться на сайте: http://lavomerka.ml/login/", inline=False)
                     await ctx.author.send(embed=embed)
                 else:
                     if register['message'] == 'Данное имя пользователя уже занято':
